664c6e5d8f4999b20fde1e502117bc9db0244301
khirwadkar_vivo14 <khirwadkar@yahoo.com>
2023-06-07 15:03:01 +0530

    Used 'git rm' command to remove locally deleted files from github repo also.

--- base_classes/member.py
+++
@@ -1,9 +0,0 @@
-class Member(object):
-    def __init__(self, type, id):
-        self.type = type    # beam, column, truss_member, etc.
-        self.id = id
-
-    def __str__(self):
-        return self.type + " " + str(self.id)
-
-
